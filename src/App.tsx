0a2e1c5c14acfd307319c31b57ca3d11c6719590
zulqarnain <abdulla.zulqarnain@stanplus.co.in>
2021-08-18 10:20:04 +0530

    todo-app finished

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoListingsPage from './views/todo-listings/todo-listings.page';
+
+function App() {
+  return (
+    <div className="App">
+        <TodoListingsPage />
+    </div>
+  );
+}
+
+export default App;
